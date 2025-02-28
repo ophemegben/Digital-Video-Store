3bb24a3cf4312e2c5a9cd744be5b42e371f3ce31
ophemegben <ndametemegbe@gmail.com>
2025-02-27 23:09:27 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import DetailsPage from './components/DetailsPage';
+import HomePage from './components/Homepage';
+import ListingPage from './components/ListingPage';
+import LoginPage from './components/LoginPage';
+import RegistrationPage from './components/RegistrationPage';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          {/* Define routes*/}
+          <Routes>
+            <Route path= "/" element={<HomePage />}/>
+            <Route path= "/details" element={<DetailsPage />}/>
+            <Route path= "/listings" element={<ListingPage />}/>
+            <Route path= "/login" element={<LoginPage />}/>
+            <Route path= "/register" element={<RegistrationPage />}/>
+          </Routes>
+        </Router>
     </div>
   );
 }
